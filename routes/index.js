e27c19f2308e210d4078ac24f2eca101e197d799
David Torosyan <dato@microsoft.com>
2017-05-13 00:51:51 -0700

    Fix sites.json path

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  require('fs').readFile('public/data/sites.json', 'utf8', function (err, data) {
-    if (err) throw err;
-    var sites = JSON.parse(data);
-    var _ = require('lodash');
-    _.mixin({
-      medianBy: function(array, iteratee) {
-        return _(array).map(iteratee).sortBy().value()[_.floor(_.size(array) / 2)]
-      }
-    });
-    var groupedSites = _(sites).sortBy('priority', 'title')
-                                .groupBy('category')
-                                .map(function(value, key) { return {'category': key, 'sites': value}})
-                                .sortBy(function(g) { return _.medianBy(g.sites, 'priority') }, 'category')
-                                .value();
-    res.render('index', { title: 'Sites', description: 'Common Internal Microsoft Portals', results: groupedSites });
+  var _ = require('lodash');
+  _.mixin({
+    medianBy: function(array, iteratee) {
+      return _(array).map(iteratee).sortBy().value()[_.floor(_.size(array) / 2)]
+    }
   });
+  var sites = require('./config/sites.json');
+  var groupedSites = _(sites).sortBy('priority', 'title')
+                              .groupBy('category')
+                              .map(function(value, key) { return {'category': key, 'sites': value}})
+                              .sortBy(function(g) { return _.medianBy(g.sites, 'priority') }, 'category')
+                              .value();
+  res.render('index', { title: 'Sites', description: 'Common Internal Microsoft Portals', results: groupedSites });
 });
 
 module.exports = router;
