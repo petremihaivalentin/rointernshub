87eef4fbffcf512809d326b02a53a8860a7ad846
David Torosyan <dato@microsoft.com>
2017-05-12 20:59:49 -0700

    Add grouping by category and sorting by priority

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,18 @@ router.get('/', function(req, res, next) {
   require('fs').readFile('public/data/sites.json', 'utf8', function (err, data) {
     if (err) throw err;
     var sites = JSON.parse(data);
-    res.render('index', { title: 'Sites', description: 'Common Internal Microsoft Portals', results: sites });
+    var _ = require('lodash');
+    _.mixin({
+      medianBy: function(array, iteratee) {
+        return _(array).map(iteratee).sortBy().value()[_.floor(_.size(array) / 2)]
+      }
+    });
+    var groupedSites = _(sites).sortBy('priority', 'title')
+                                .groupBy('category')
+                                .map(function(value, key) { return {'category': key, 'sites': value}})
+                                .sortBy(function(g) { return _.medianBy(g.sites, 'priority') }, 'category')
+                                .value();
+    res.render('index', { title: 'Sites', description: 'Common Internal Microsoft Portals', results: groupedSites });
   });
 });
 
