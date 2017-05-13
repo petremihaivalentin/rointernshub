8d3a170f2d73584f06bd06504f252ffa68d80435
David Torosyan <dato@microsoft.com>
2017-05-12 19:51:11 -0700

    Add basic site listing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  require('fs').readFile('public/data/sites.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    var sites = JSON.parse(data);
+    res.render('index', { title: 'Sites', description: 'Common Internal Microsoft Portals', results: sites });
+  });
 });
 
 module.exports = router;
