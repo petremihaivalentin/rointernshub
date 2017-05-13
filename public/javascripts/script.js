58afc5dae0193bb822c99cbb232f6e35caf71fe4
David Torosyan <dato@microsoft.com>
2017-05-12 23:24:33 -0700

    Add checkbox to disable animations

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,40 +2,41 @@
 
 function filter_tiles(searchBar) {
     var search = searchBar.value.toLowerCase();
+    var animationsEnabled = $("#animationsEnabled").is(":checked");
 
     // filter listings
     _.forEach($('.listing'), function(listing) {
         if (!_.includes($(listing).text(), search)) {
-            $(listing).gentleSlideOut();
+            $(listing).gentleSlideOut(animationsEnabled);
         }
         else {
-            $(listing).gentleSlideIn();
+            $(listing).gentleSlideIn(animationsEnabled);
         }
     })
 
     // hide empty categories
     _.forEach($('.categorySection'), function(category) {
         if (_.isEmpty($(category).find('.listing:not(.filtered)'))) {
-            $(category).gentleFadeOut();
+            $(category).gentleFadeOut(animationsEnabled);
         }
         else {
-            $(category).gentleFadeIn();
+            $(category).gentleFadeIn(animationsEnabled);
         }
     })
 
     // notify if no results
     if (_.isEmpty($("#main").find('.categorySection:not(.filtered)'))) {
-        $('#noresults').gentleFadeIn();
+        $('#noresults').gentleFadeIn(animationsEnabled);
     }
     else {
-        $('#noresults').gentleFadeOut();
+        $('#noresults').gentleFadeOut(animationsEnabled);
     }
 }
 
 /* extend jquery with gentle animations */
 
 (function( $ ){
-    $.fn.gentleSlideOut = function() {
+    $.fn.gentleSlideOut = function(animationsEnabled) {
         if ($(this).hasClass('filtered')) {
             return this;
         }
@@ -47,14 +48,14 @@ function filter_tiles(searchBar) {
             'margin-right': '0px',
             'padding-left': '0px'
         }, {
-            duration: 400,
+            duration: animationsEnabled ? 400 : 0,
             complete: function () {
                 $(this).hide();
             }
         });
         return this;
     };
-    $.fn.gentleSlideIn = function() {
+    $.fn.gentleSlideIn = function(animationsEnabled) {
         if (!$(this).hasClass('filtered')) {
             return this;
         }
@@ -67,11 +68,11 @@ function filter_tiles(searchBar) {
             'margin-right': '20px',
             'padding-left': '15px'
         }, {
-            duration: 400
+            duration: animationsEnabled ? 400 : 0,
         });
         return this;
     }; 
-    $.fn.gentleFadeOut = function() {
+    $.fn.gentleFadeOut = function(animationsEnabled) {
         if ($(this).hasClass('filtered')) {
             return this;
         }
@@ -80,14 +81,14 @@ function filter_tiles(searchBar) {
         this.animate({
             opacity: 0
         }, {
-            duration: 400,
+            duration: animationsEnabled ? 400 : 0,
             complete: function () {
                 $(this).hide();
             }
         });
         return this;
     }; 
-    $.fn.gentleFadeIn = function() {
+    $.fn.gentleFadeIn = function(animationsEnabled) {
         if (!$(this).hasClass('filtered')) {
             return this;
         }
@@ -97,7 +98,7 @@ function filter_tiles(searchBar) {
         this.animate({
             opacity: 1
         }, {
-            duration: 400
+            duration: animationsEnabled ? 400 : 0,
         });
         return this;
     }; 
