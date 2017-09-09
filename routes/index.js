1a3b53be62fc8856b60909b575007d7898517276
David Torosyan <dato@microsoft.com>
2017-09-09 19:34:02 +0000

    Merged PR 13: Add analytics
    * Add analytics
    * Cleanup code
    * Visual Studio support

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  var _ = require('lodash');
-  _.mixin({
-    medianBy: function(array, iteratee) {
-      return _(array).map(iteratee).sortBy().value()[_.floor(_.size(array) / 2)]
-    }
-  });
-  var sites = require('./config/sites.json');
-  var groupedSites = _(sites).sortBy('priority', 'title')
-                              .groupBy('category')
-                              .map(function(value, key) { return {'category': key, 'sites': value}})
-                              .sortBy(function(g) { return _.medianBy(g.sites, 'priority') }, 'category')
-                              .value();
-  res.render('index', { title: 'mssites', description: 'Common Internal Microsoft Portals', results: groupedSites });
+// GET home page
+router.get('/', function (req, res, next) {
+    // setup lodash
+    var _ = require('lodash');
+    _.mixin({
+        medianBy: (array, iteratee) => _(array).map(iteratee).sortBy().value()[_.floor(_.size(array) / 2)]
+    });
+
+    // render sites, grouped by category and sorted by priority
+    var sites = require('./config/sites.json');
+    var sitesGroupedByCategory = _(sites)
+        .sortBy('priority', 'title')
+        .groupBy('category')
+        .map((value, key) => ({'category': key, 'sites': value}))
+        .sortBy(g => _.medianBy(g.sites, 'priority'), 'category')
+        .value();
+
+    res.render('index', { title: 'mssites', description: 'Common Internal Microsoft Portals', sitesGroupedByCategory: sitesGroupedByCategory });
 });
 
 module.exports = router;
